0375b0e64213abaeeb39bfbc9f81c4e7f2427a03
omeyemburg <oskar.git@gmx.net>
2025-06-20 22:27:08 +0200

    chore(nix): add flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Build tools";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            libnsl
+            xorg.libX11
+            xorg.libXext
+          ];
+
+          shellHook = ''
+            export SHELL=${pkgs.bashInteractive}/bin/bash
+          '';
+        };
+      }
+    );
+}
